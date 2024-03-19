acf3c60490806a0a6a2096af9c9351298bca1470
Guilherme Silva <2003silvagui@gmail.com>
2024-03-19 16:07:57 -0300

    feat: animation page login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Login() {
     <>
       <NextUIProvider>
         <main className="w-screen h-screen flex justify-center items-center lg:bg-[#9377ef2b]">
-          <div className="w-full flex justify-center xs:w-96 sm:shadow py-10 rounded-2xl lg:hidden">
+          <div className="w-full flex justify-center xs:w-96 sm:shadow py-10 rounded-2xl lg:hidden animate-fade-down animate-once">
             <FormLogin />
           </div>
-          <div className="hidden lg:flex w-[900px] h-5/6 shadow-2xl rounded-2xl bg-white">
+          <div className="hidden lg:flex w-[900px] h-5/6 shadow-2xl rounded-2xl bg-white animate-fade-down animate-once">
             <div className="w-6/12 flex to-[#9377EF] from-[#6e4ed6] bg-gradient-to-r justify-center items-center rounded-2xl">
               <FormLogin />
             </div>
